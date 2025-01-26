f4fee190c1e6458a451e2fd7a9438f2d3a767c0a
lakhavim <lakhavim@sheridancollege.ca>
2025-01-26 17:32:55 -0500

    versio 1

--- src/week1/Week1.java
+++ src/week1/Week1.java
@@ -16,6 +16,7 @@
                     public static void main(String[] args) {
 
                         System.out.println("hello");    
+                        System.out.println("world");
                         // TODO code application logic here
                     }
 
